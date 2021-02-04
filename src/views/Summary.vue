39975c53e836f85a89b72baa70f59d566d8481fc
Angelique Weger <angelique@lumenlearning.com>
2021-02-03 20:52:52 -0500

    Displays score as percentage in summary

--- src/views/Summary.vue
+++ src/views/Summary.vue
@@ -1,8 +1,11 @@
 <template>
+  <h2>Your Score</h2>
+  <h3 class="u-margin-top-none u-margin-bottom-lg u-margin-left">{{ getScore }}</h3>
   <QuestionList :data="quizData" />
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import QuestionList from '@/components/QuestionList.vue';
 export default {
   components: {
@@ -13,7 +16,25 @@ export default {
       quizData: this.$store.state.quiz.quizData,
     };
   },
+  computed: {
+    ...mapGetters({
+      listQuestions: 'quiz/listQuestions',
+      countQuestions: 'quiz/countQuestions',
+      countCorrect: 'quiz/countCorrect',
+      getScore: 'quiz/calcScore',
+    }),
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h2 {
+  font: {
+    size: $text-size-up-3;
+    weight: 400;
+  }
+}
+h3 {
+  font-size: $text-size-up-4;
+}
+</style>
