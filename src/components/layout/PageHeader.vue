d6c5f589538992d0f60356617eac240835071b07
Angelique Weger <angelique@lumenlearning.com>
2021-02-03 20:41:32 -0500

    Adds quiz title to UI

---
+++ src/components/layout/PageHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1 class="u-margin-none u-padding-sm">{{ getTitle }}</h1>
+  </header>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'AppHeader',
+  computed: {
+    ...mapGetters({
+      getTitle: 'quiz/quizTitle',
+    }),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  background-color: $color-ebony-clay;
+  color: white;
+  font-size: $text-size-up-5;
+}
+</style>
